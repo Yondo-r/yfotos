4b8919054fdd2f29b09ca7a61befefbfaf6292e8
yondo-r <yondo.ricardo@gmail.com>
2018-06-02 15:30:08 -0300

    criado select de user, colocado picture input e feita funcao do adm add foto

--- yfotos/src/components/HelloWorld.vue
+++ yfotos/src/components/HelloWorld.vue
@@ -1,113 +1,110 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
+  <v-app>
+    <v-card-text>
+    <div>
+      <v-container wrap align-center>
+        <v-select
+          :items="selectPerson"
+          v-model="selectPersonModel"
+          label="Informe o tipo de usuário"
+          autocomplete
+        ></v-select>
+      </v-container>
+    </div>
+    </v-card-text>
+    <div v-if="selectPersonModel == 'admin'">
+      <v-flex text-xs-center>
+        <v-btn @click="fotosModel=true"> Fotos</v-btn>
+      </v-flex>
+      <div v-if="fotosModel == false">
+        <picture-input
+          ref="pictureInput"
+          @change="onChange"
+          width="250"
+          height="250"
+          margin="16"
+          accept="image/jpeg,image/png"
+          size="5"
+          buttonClass="btn"
+          :customStrings="{
+            upload: '<h1>Bummer!</h1>',
+            drag: 'Clique ou arraste uma imagem aqui 😺 '
+          }">
+        </picture-input>
+        <v-flex text-xs-center>
+          <v-btn @click="addFoto()">Adicionar foto</v-btn>
+        </v-flex>
+      </div>
+    </div>
+    <!-- MODAL DE FOTOS -->
+    <v-dialog v-model="fotosModel" max-width="790">
+      <v-card>
+        <v-card-title class="headline">As fotos serão exibidas aqui</v-card-title>
+        <v-container v-bind="{ [`grid-list-md`]: true }" fluid>
+          <v-layout row wrap>
+            <v-flex
+              v-for="(n, index) in arrayDeFotos"
+              :key="index"
+              xs4
+            >
+              <v-card flat tile>
+                <div>
+                  <v-card-media
+                    :src= arrayDeFotos[index]
+                    height="250px"
+                  >
+                  </v-card-media>
+                </div>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="green darken-1" flat="flat" @click.native="dialog = false">Fechar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-app>
 </template>
 
 <script>
+/* eslint-disable */
+import PictureInput from 'vue-picture-input'
 export default {
   name: 'HelloWorld',
   data() {
     return {
-      msg: 'Ricardo, Welcome to Your Vue.js App',
+      fotosModel: false,
+      selectPersonModel: null,
+      selectPerson: ['admin', 'representante', 'usuário'],
+      arrayDeFotos: [],
+      image: '',
     };
   },
+  components: {
+    PictureInput
+  },
+  methods: {
+    onChange (image) {
+      console.log('New picture selected!')
+      if (image) {
+        console.log('Picture loaded.')
+        this.image = image
+      } else {
+        console.log('FileReader API not supported: use the <form>, Luke!')
+      }
+    },
+    addFoto(){
+      this.arrayDeFotos.push(this.image)
+      console.log("abaixo devemos exibir o array de fotos")
+      console.log(this.arrayDeFotos)
+    }
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
--- yfotos/src/main.js
+++ yfotos/src/main.js
@@ -1,5 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'vuetify/dist/vuetify.min.css';
+import Vuetify from 'vuetify';
 import Vue from 'vue';
 import App from './App';
 import router from './router';
@@ -7,6 +9,7 @@ import router from './router';
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
+Vue.use(Vuetify);
 new Vue({
   el: '#app',
   router,
